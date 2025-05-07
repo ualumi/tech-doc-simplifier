846233d2526a6034c9b788a66f226161b3fbc192
ualumi <zroblukavy@gmail.com>
2025-05-07 10:30:52 +0300

    model-service updated
    запускается и пытается отправить сообщение но падает( кеш в text-service работает если его restart

--- model-service/kafka_consumer.py
+++ model-service/kafka_consumer.py
@@ -1,10 +1,10 @@
 import os
-from kafka import KafkaConsumer
 import json
+from kafka import KafkaConsumer
 from model.simplifier import simplify_text
 from kafka_producer import send_result_to_kafka
 
-KAFKA_BROKER = os.getenv("KAFKA_BROKER", "localhost:9092")
+KAFKA_BROKER = os.getenv("KAFKA_BROKER", "broker:9092")
 REQUEST_TOPIC = os.getenv("KAFKA_REQUEST_TOPIC", "model_requests")
 
 consumer = KafkaConsumer(
@@ -20,16 +20,16 @@ def consume():
     print(f"Listening to '{REQUEST_TOPIC}' on {KAFKA_BROKER}...")
     for message in consumer:
         request = message.value
-        print(f"Received: {request}")
+        correlation_id = message.key.decode('utf-8') if message.key else None
+        print(f"Received: key={correlation_id}, value={request}")
         
         text = request.get("text")
-        token = request.get("token")  # correlation ID для ответа
+        token = request.get("token")
 
-        if text and token:
+        if text and correlation_id:
             simplified = simplify_text(text)
             result = {
                 "original": {"text": text, "token": token},
                 "simplified": {"text": simplified, "token": token}
             }
-            send_result_to_kafka(result, correlation_id=token)
-
+            send_result_to_kafka(result, correlation_id=correlation_id)
--- model-service/kafka_producer.py
+++ model-service/kafka_producer.py
@@ -2,7 +2,7 @@ import os
 from kafka import KafkaProducer
 import json
 
-KAFKA_BROKER = os.getenv("KAFKA_BROKER", "localhost:9092")
+KAFKA_BROKER = os.getenv("KAFKA_BROKER", "broker:9092")
 RESPONSE_TOPIC = os.getenv("KAFKA_RESPONSE_TOPIC", "model_response")
 
 producer = KafkaProducer(
@@ -10,7 +10,13 @@ producer = KafkaProducer(
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
+# kafka_producer.py
 def send_result_to_kafka(result, correlation_id):
-    print(f"Sending to '{RESPONSE_TOPIC}': key={correlation_id}, value={result}")
-    producer.send(RESPONSE_TOPIC, key=correlation_id.encode('utf-8'), value=result)
-    producer.flush()
+    print(f"Sending model response to topic: key={correlation_id}, value={result}")
+    producer.send(
+        RESPONSE_TOPIC,
+        key=correlation_id.encode('utf-8'),
+        value=json.dumps(result).encode('utf-8')
+    )
+
+
